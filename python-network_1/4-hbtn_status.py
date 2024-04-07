6bb43e63342907f3f35cb8260ea34c7612832880
phinah <phinah.mahoro10@gmail.com>
2024-04-07 18:20:46 +0200

    fix: added the right link

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """__summary__
 - Write a Python script that
-- fetches https://intranet.hbtn.io/status.
+- fetches https://alu-intranet.hbtn.io/status.
 """
 import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get("https://alu-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
